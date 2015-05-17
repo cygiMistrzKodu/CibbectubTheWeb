a43fba0e1d18fc51f1b9a628930a0b5210a6ba4b
JacekMistrzuKoder <jacek301@gmail.com>
2015-05-17 17:11:55 +0200

    Inne Przyklady Jsup

--- ConnectingTheWeb/src/test/java/com/cygi/webService/ConnectingTheWeb/TestHtmlParser.java
+++ ConnectingTheWeb/src/test/java/com/cygi/webService/ConnectingTheWeb/TestHtmlParser.java
@@ -1,13 +1,16 @@
 package com.cygi.webService.ConnectingTheWeb;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 public class TestHtmlParser {
@@ -20,21 +23,41 @@ public class TestHtmlParser {
 		String path = "D:\\PogodaPrzetwarzanie\\StronaPogoda.html";
 		
 		
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
+//		byte[] encoded = Files.readAllBytes(Paths.get(path));
+//		
+//		String htmlPage =  new String(encoded, Charset.defaultCharset());
 		
-		String htmlPage =  new String(encoded, Charset.defaultCharset());
+	//	Document doc = Jsoup.parseBodyFragment(htmlPage);	
 		
-		Document doc = Jsoup.parseBodyFragment(htmlPage);
+	//	Element body = doc.body();
 		
-		Element body = doc.body();
+		File input = new File(path);
+		
+		Document doc = Jsoup.parse(input, "UTF-8");
+		
+		Elements content = doc.getElementsByAttribute("title");
+		
+//		System.out.println(content);
+//		System.out.println("-------------------------------------------");		
+		
+		for (Element htmlElement : content)
+		{
+				System.out.println("************************************************");
+			System.out.println(htmlElement);
+			System.out.println(htmlElement.tagName());
+			System.out.println(htmlElement.outerHtml());
+			System.out.println("--------------------------------------------------------");
+			
+			for (org.jsoup.nodes.Attribute attributes : htmlElement.attributes()) {
 				
+				System.out.println(attributes);
 				
-		// pandora pandora		klip klap  dup dupa dupa merge
+			}
+			
+			System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		}
 		
-				// juz nie bla bla juz jestem w drugim eklipise bum bum
-				
-	//	System.out.println(htmlPage);
-		System.out.println(body);
+	//	System.out.println(body);
 		
 	}
 
