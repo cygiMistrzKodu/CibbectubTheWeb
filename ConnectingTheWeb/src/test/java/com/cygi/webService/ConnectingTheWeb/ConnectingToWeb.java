504a9afc3f299c1fd39330aee4a2f3d6feda05f4
JacekMistrzuKoder <jacek301@gmail.com>
2015-05-17 20:38:48 +0200

    Add lerning Test hwo to use Jsop html parser
    Dwonload site
    parse html and take temperature from site
    and display it in consoele

---
+++ ConnectingTheWeb/src/test/java/com/cygi/webService/ConnectingTheWeb/ConnectingToWeb.java
@@ -0,0 +1,40 @@
+package com.cygi.webService.ConnectingTheWeb;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+public class ConnectingToWeb {
+
+	@Test
+	public void takeWhetherTemperatureFromSiteAndDisplayItForTwonWroclaw() throws IOException {
+		
+		String site = "http://www.twojapogoda.pl/";
+		
+		
+		
+		Document doc = Jsoup.connect(site).get();
+		
+		Elements tableWithWether = doc.select("div.map-wrapper");
+		
+		//Elements wroclawTown = tableWithWether.select("p.name:contains(Wrocław)");
+		Elements wroclawTown = tableWithWether.select("a:contains(Wrocław)");
+		
+		//String title = doc.body().html();
+		
+		Elements temperature = wroclawTown.select("p.value");
+		
+	//	System.out.println(wroclawTown);
+		System.out.println(temperature.text());
+		//System.out.println(tableWithWether);
+		
+		System.out.println("Temperatura we Wrocławiu : " + temperature.text());
+		
+	}
+
+}
